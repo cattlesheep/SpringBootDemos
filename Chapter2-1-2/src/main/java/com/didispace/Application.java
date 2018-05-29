8565e3d8f9379fb431af7edca23918b1ae129202
cattlesheep <972240594@qq.com>
2018-05-29 14:55:40 +0800

    SpringBoot学习：SpringBoot2.0属性绑定

--- Chapter2-1-2/src/main/java/com/didispace/Application.java
+++ Chapter2-1-2/src/main/java/com/didispace/Application.java
@@ -1,6 +1,8 @@
 package com.didispace;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 @Slf4j
 @SpringBootApplication
 public class Application {
-
+    private static final Log log = LogFactory.getLog(Application.class);
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
